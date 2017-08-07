a8cdd6fca27906504b77cb4c4b605b9b67e3ac42
DESKTOP-JM3HH2O\jane1 <jane199141@gmail.com>
2017-08-07 17:14:31 +0800

    2017.08.07

---
+++ Bookkeeping/Bookkeeping/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Bookkeeping.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Bookkeeping.Controllers
+{
+    public class HomeController : Controller
+    { 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Bookkeeping()
+        {
+            return View(new BookkeepingHeaderViewModel());
+        }
+
+        [ChildActionOnly] //避免這個Action被外部連入
+        public ActionResult BookkeepingList()
+        {
+            List<BookkeepingMemoListViewModel> bookkeepingList = new List<BookkeepingMemoListViewModel>();
+            for (int countDays = 0; countDays < 5; countDays++)
+            {
+                bookkeepingList.Add(new BookkeepingMemoListViewModel()
+                {
+                    Types = "支出",
+                    DateTimes = DateTime.Now.AddDays(-countDays),
+                    Money = 10 * Convert.ToInt16(DateTime.Now.Day) * (countDays+1)
+                });
+            }
+            return View(bookkeepingList);
+        }
+    }
+}
---
+++ Bookkeeping/Bookkeeping/Models/ViewModels/BookkeepingViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookkeeping.Models.ViewModels
+{
+    public class BookkeepingHeaderViewModel
+    {
+        [DisplayName("金額")]
+        public int Money { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayName("日期")]
+        public DateTime DateTimes { get; set; }
+
+        [DisplayName("備註")]
+        public string Notes { get; set; }
+    }
+
+    public class BookkeepingMemoListViewModel
+    {
+        [DisplayName("#")]
+        public int ItemsCount { get; set; }
+
+        [DisplayName("類別")]
+        public string Types { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayName("日期")]
+        public DateTime DateTimes { get; set; }
+
+        [DisplayName("金額")]
+        public int Money { get; set; }
+    }
+}
