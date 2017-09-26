fb1e1cd771d373aa2042ba6231b95857563bf95c
DESKTOP-JM3HH2O\jane1 <jane199141@gmail.com>
2017-09-26 17:17:42 +0800

    訂正第三次作業

--- Bookkeeping/Bookkeeping/Models/Partials/BookInfo.cs
+++ Bookkeeping/Bookkeeping/Models/Partials/BookInfo.cs
@@ -1,11 +1,9 @@
-﻿using Bookkeeping.Enum;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace Bookkeeping.Models
 {
@@ -20,7 +18,7 @@ namespace Bookkeeping.Models
             [DisplayName("序號")]
             [Required(ErrorMessage ="請輸入序號")]
             public Guid Id { get; set; }
-                
+             
             [DisplayName("類別")]
             [Required(ErrorMessage = "請輸入類別")]
             public int Types { get;set;}
@@ -29,13 +27,11 @@ namespace Bookkeeping.Models
             [Required(ErrorMessage = "請輸入金額")]
             public int Money { get; set; }
 
-            [UIHint("DateTime")]
             [DisplayName("日期")]
             [Required(ErrorMessage = "請輸入日期")]
             public DateTime DateTimes { get; set; }
 
             [DisplayName("備註")]
-            [Remote("CheckUserName", "Validate", ErrorMessage = "遠端驗證失敗")]
             [StringLength(50)]
             public string Notes { get; set; }
         }
--- Bookkeeping/Bookkeeping/Models/ViewModels/BookkeepingViewModel.cs
+++ Bookkeeping/Bookkeeping/Models/ViewModels/BookkeepingViewModel.cs
@@ -3,8 +3,6 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using PagedList;
 using Bookkeeping.Attribute;
-using Bookkeeping.Enum;
-using System.Web.Mvc;
 
 namespace Bookkeeping.Models.ViewModels
 {
@@ -25,24 +23,23 @@ namespace Bookkeeping.Models.ViewModels
     /// </summary>
     public class BookkeepingHeaderViewModel
     {
-        [Required(ErrorMessage = "{0} 必須為必填")]
+        [Required(ErrorMessage ="{0} 必須為必填")]
         [DisplayName("金額")]
-        [RegularExpression(@"^(0|[1-9][0-9]*)$", ErrorMessage = "請輸入正整數")]
+        [RegularExpression(@"^(0|[1-9][0-9]*)$",ErrorMessage ="請輸入正整數")]
         public int Money { get; set; }
-
+         
         [Required(ErrorMessage = "{0} 必須為必填")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")] 
         [DisplayName("日期")]
-        [BookInfoDateTimeRange(ErrorMessage = "日期不得大於今日")]
+        [BookInfoDateTimeRange(ErrorMessage ="日期不得大於今日")] 
         public DateTime DateTimes { get; set; }
 
         [Required(ErrorMessage = "{0} 必須為必填")]
-        [MaxLength(100, ErrorMessage = "{0} 最大不得過 {1} 個字")]
-        [Remote("CheckUserName", "Home", ErrorMessage = "不可輸入demoshop")]
+        [MaxLength(100,ErrorMessage ="{0} 最大不得過 {1} 個字")]
         [DisplayName("備註")]
         public string Notes { get; set; }
 
-        [DisplayName("類別")] 
+        [DisplayName("類別")]
         [Required(ErrorMessage = "請輸入類別")]
         public int Types { get; set; }
     }
@@ -64,6 +61,6 @@ namespace Bookkeeping.Models.ViewModels
 
         [DisplayName("金額")]
         public int Money { get; set; }
-
+         
     }
 }
